439b463b764fee10878098645d5ba955ca1d4108
ww <17600626753@163.com>
2023-06-21 14:27:14 +0800

    init: 添加白名单

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,7 +5,11 @@ const routes = [
     component: Home,
     name: 'home',
   },
-  { path: '/login', component: import('@/views/login/index.vue'), name: 'login' },
+  {
+    path: '/login',
+    component: import('@/views/login/index.vue'),
+    name: 'login',
+  },
   { path: '/404', component: import('@/views/404/index.vue'), name: '404' },
   // { path: '/:pathMatch(.*)*', redirect: '/404', name: 'Home' },
 ]
